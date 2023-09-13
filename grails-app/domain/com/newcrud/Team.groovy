d0f179acc1c55578095bc5a465131251fef6f014
harshjoshi23 <harsh.joshi23@gmail.com>
2023-09-13 10:44:26 +0200

    first commit

---
+++ grails-app/domain/com/newcrud/Team.groovy
@@ -0,0 +1,19 @@
+package com.newcrud
+
+class Team {
+
+    static belongsTo = [team: Team]
+    String name
+    Integer age
+    String position
+
+    static constraints = {
+        name size: 5..40, blank: false
+        age min: 16
+        position size: 5..40, blank: false
+    }
+
+    String toString() {
+        name
+    }
+}
---
+++ grails-app/init/com/newcrud/BootStrap.groovy
@@ -0,0 +1,9 @@
+package com.newcrud
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
